aaa9e5bd85f43f91e414d3f8f3c145c6a4b23135
Reza Mansouri <itsrezamansouri@gmail.com>
2024-05-23 15:43:31 -0400

    add new imax sunrise

---
+++ src/experiments/imax_sunrise/utils/analysis.py
@@ -0,0 +1,11 @@
+import torch
+import numpy as np
+
+
+def get_weights(masks):
+    masks = np.array(masks)
+    classes, counts = np.unique(masks, return_counts=True)
+    fqs = counts / sum(counts)
+    fqs = {k: v for k, v in zip(classes, fqs)}
+    w = torch.tensor([1/fqs[c] for c in sorted(classes)])
+    return w / max(w)
