3c9f2f64cb6da60b224ccf40251325732e917fea
Reza Mansouri <itsrezamansouri@gmail.com>
2024-05-27 18:20:16 -0400

    up

--- src/experiments/imax_sunrise_2_phase/phase_1_identify/utils/frontiers_metrics.py
+++ src/experiments/imax_sunrise_2_phase/phase_1_identify/utils/frontiers_metrics.py
@@ -16,6 +16,7 @@ def nanmean(x):
 
 def _fast_hist(true, pred, num_classes):
     mask = (true >= 0) & (true < num_classes)
+    true = true.type(torch.int64)
     hist = torch.bincount(
         num_classes * true[mask] + pred[mask],
         minlength=num_classes ** 2,
