2c74216bfc6c503adf66a7d17d7148dc2c5c94ce
Reza Mansouri <itsrezamansouri@gmail.com>
2024-06-17 18:26:36 -0400

    add dropout to attunet

--- src/experiments/imax_sunrise_1_phase/models/models.py
+++ src/experiments/imax_sunrise_1_phase/models/models.py
@@ -302,6 +302,8 @@ class AttU_Net(nn.Module):
         self.Up_conv2 = conv_block(ch_in=128, ch_out=64)
 
         self.Conv_1x1 = nn.Conv2d(64,output_ch,kernel_size=1,stride=1,padding=0)
+        
+        self.drop_out = nn.Dropout2d()
 
 
     def forward(self,x):
@@ -309,34 +311,42 @@ class AttU_Net(nn.Module):
         x1 = self.Conv1(x)
 
         x2 = self.Maxpool(x1)
+        x2 = self.drop_out(x2)
         x2 = self.Conv2(x2)
         
         x3 = self.Maxpool(x2)
+        x3 = self.drop_out(x3)
         x3 = self.Conv3(x3)
 
         x4 = self.Maxpool(x3)
+        x4 = self.drop_out(x4)
         x4 = self.Conv4(x4)
 
         x5 = self.Maxpool(x4)
+        x5 = self.drop_out(x5)
         x5 = self.Conv5(x5)
 
         # decoding + concat path
         d5 = self.Up5(x5)
+        d5 = self.drop_out(d5)
         x4 = self.Att5(g=d5,x=x4)
         d5 = torch.cat((x4,d5),dim=1)        
         d5 = self.Up_conv5(d5)
         
         d4 = self.Up4(d5)
+        d4 = self.drop_out(d4)
         x3 = self.Att4(g=d4,x=x3)
         d4 = torch.cat((x3,d4),dim=1)
         d4 = self.Up_conv4(d4)
 
         d3 = self.Up3(d4)
+        d3 = self.drop_out(d3)
         x2 = self.Att3(g=d3,x=x2)
         d3 = torch.cat((x2,d3),dim=1)
         d3 = self.Up_conv3(d3)
 
         d2 = self.Up2(d3)
+        d2 = self.drop_out(d2)
         x1 = self.Att2(g=d2,x=x1)
         d2 = torch.cat((x1,d2),dim=1)
         d2 = self.Up_conv2(d2)
