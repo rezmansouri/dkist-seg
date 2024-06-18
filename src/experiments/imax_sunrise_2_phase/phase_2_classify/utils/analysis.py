0c5fab6b50833896b54c4c5c4c3535ccfb9b29fb
Reza Mansouri <itsrezamansouri@gmail.com>
2024-06-17 21:33:50 -0400

    static loss weights

--- src/experiments/imax_sunrise_2_phase/phase_2_classify/utils/analysis.py
+++ src/experiments/imax_sunrise_2_phase/phase_2_classify/utils/analysis.py
@@ -3,9 +3,10 @@ import numpy as np
 
 
 def get_weights(masks):
-    masks = np.array(masks)
-    classes, counts = np.unique(masks, return_counts=True)
-    fqs = counts / sum(counts)
-    fqs = {k: v for k, v in zip(classes, fqs)}
-    w = torch.tensor([1/fqs[c] for c in sorted(classes)])
-    return w / max(w)
+    # masks = np.array(masks)
+    # classes, counts = np.unique(masks, return_counts=True)
+    # fqs = counts / sum(counts)
+    # fqs = {k: v for k, v in zip(classes, fqs)}
+    # w = torch.tensor([1/fqs[c] for c in sorted(classes)])
+    # return w / max(w)
+    return torch.tensor([1.0, 10.0, 10.0, 10.0, 1.0])
